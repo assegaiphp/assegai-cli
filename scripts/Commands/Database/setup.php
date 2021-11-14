440d8074aba40ccb937b25e1f8a83d196c885094
Andrew Masiye <amasiye313@gmail.com>
2021-11-14 20:48:18 +0200

    feat(database): implement setup command
    * add logic for database setup

--- scripts/Commands/Database/setup.php
+++ scripts/Commands/Database/setup.php
@@ -1,4 +1,89 @@
 #!/usr/bin/php
 <?php
 
-echo "setup\n";
+namespace Assegai\Commands\Database;
+
+use Assegai\LIB\DatabaseSelector;
+use Assegai\LIB\Logging\Logger;
+
+list($type, $name) = match(count($args)) {
+  1       => [null, null],
+  2       => [$args[1], null],
+  default => array_slice($args, 1),
+};
+
+$databaseSelector = new DatabaseSelector(
+  databaseType: $type,
+  databaseName: $name,
+  promptToCreate: true
+);
+$databaseSelector->run();
+
+$selectedDatabaseType = $databaseSelector->databaseType();
+$selectedDatabaseName = $databaseSelector->databaseName();
+
+# If database doesn't exist, create it
+
+$connection           = $databaseSelector->connection();
+
+$migrationsDir = "$workingDirectory/migrations/$selectedDatabaseType/$selectedDatabaseName";
+$initialMigrationDir = "$migrationsDir/00000000000000_initial";
+
+if (!file_exists($initialMigrationDir))
+{
+  if (mkdir(directory: $initialMigrationDir, recursive: true))
+  {
+    Logger::logCreate(path: str_replace("$workingDirectory/", '', $initialMigrationDir));
+  }
+  else
+  {
+    Logger::error(message: "Failed to create $initialMigrationDir");
+  }
+}
+
+if (!file_exists($initialMigrationDir . '/up.sql'))
+{
+  if (fopen(filename: $initialMigrationDir . '/up.sql', mode: 'w'))
+  {
+    Logger::logCreate(path: str_replace("$workingDirectory/", '', $initialMigrationDir . '/up.sql'));
+  }
+  else
+  {
+    Logger::error(message: "Failed to create $initialMigrationDir/up.sql");
+  }
+}
+
+if (!file_exists($initialMigrationDir . '/down.sql'))
+{
+  if (fopen(filename: $initialMigrationDir . '/down.sql', mode: 'w'))
+  {
+    Logger::logCreate(path: str_replace("$workingDirectory/", '', $initialMigrationDir . '/down.sql'));
+  }
+  else
+  {
+    Logger::error(message: "Failed to create $initialMigrationDir/down.sql");
+  }
+}
+
+# 1. Create database if it doesn't exist
+
+# 2. Create migrations table
+$migrationsTableSchema = "CREATE TABLE IF NOT EXISTS `$selectedDatabaseName`.`__assegai_schema_migrations` (
+  `migration` VARCHAR(14) NOT NULL ,
+  `ran_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`migration`)) ENGINE = InnoDB";
+
+$statement = $connection->query($migrationsTableSchema);
+
+if (is_bool($statement))
+{
+  exit($connection->errorInfo());
+}
+
+if (!$statement->execute())
+{
+  exit($connection->errorInfo());
+}
+
+Logger::logCreate('__assegai_schema_migrations table');
+Logger::log(message: "\nAll done!");
