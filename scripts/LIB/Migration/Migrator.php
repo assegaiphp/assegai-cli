e5ea73a4f5e2d411bf45b417c30b88e6a038c9ad
Andrew Masiye <amasiye313@gmail.com>
2021-11-15 04:41:01 +0200

    feat(migration): implement run method
    * implement run method for Migrator class

--- scripts/LIB/Migration/Migrator.php
+++ scripts/LIB/Migration/Migrator.php
@@ -3,18 +3,30 @@
 
 namespace Assegai\LIB\Migration;
 
+use Assegai\LIB\Color;
 use Assegai\LIB\Logging\Logger;
 use PDO;
+use PDOException;
 
 /**
  * The `Migrator` class provides methods for maintaining database schema.
  * 
- * A database migration always provides procedures to update the schema, as well as to revert itself. Assegai's migrations are versioned, and run in order. Assegai also takes care of tracking which migrations have already been run automatically. Your migrations don't need to be idempotent, as Assegai will ensure no migration is run twice unless it has been reverted.
+ * A database migration always provides procedures to update the schema, 
+ * as well as to revert itself. Assegai's migrations are versioned, and 
+ * run in order. Assegai also takes care of tracking which migrations have 
+ * already been run automatically. Your migrations don't need to be 
+ * idempotent, as Assegai will ensure no migration is run twice unless it 
+ * has been reverted.
  * 
  * @author A. Masiye <amasiye313@gmail.com>
  */
 final class Migrator
 {
+  const RUN_ALL     = 'run_all';
+  const RUN_NEXT    = 'run_next';
+  const REVERT_ALL  = 'revert_all';
+  const REVERT_NEXT = 'revert_next';
+
   private string $databaseType = 'mysql';
   private string $tableName = '';
 
@@ -68,27 +80,63 @@ final class Migrator
     }
   }
 
-  public function run(): void
+  public function run(string $mode = self::RUN_ALL): void
   {
-    // $statement = $this->connection->query("INSERT INTO __assegai_schema_migrations (`migration`) VALUES('$name')");
+    $runCount = 0;
+    $migrations = $this->listMigrations();
 
-    // if ($statement === false)
-    // {
-    //   Logger::error(message: implode("\n", $this->connection->errorInfo()), terminateAfterLog: true);
-    // }
+    try
+    {
+      $ranMigrations = [];
+      foreach ($migrations as $timestamp => $migration)
+      {
+        ++$runCount;
+        if (is_null($migration->ranOn()))
+        {
+          # Run the up.sql file
+          $upFilename = sprintf("%s/%s/up.sql", $this->migrationDirectory, $migration->name());
+          $contents = file_get_contents($upFilename);
+          $statement = $this->connection->query($contents);
 
-    Logger::logUpdate(path: '__assegai_schema_migrations');
+          if ($statement === false)
+          {
+            Logger::error(message: sprintf("Failed to run %s", $migration->name()), terminateAfterLog: true);
+          }
+
+          $ranMigrations[$migration->value()] = $migration;
+
+          # Record migration
+          $migrationValue = $migration->value();
+          $recordStatement = "INSERT INTO `__assegai_schema_migrations` (`migration`) VALUES('$migrationValue')";
+          $recordStatement = $this->connection->query($recordStatement);
+          if ($statement === false)
+          {
+            Logger::error(message: implode("\n", $this->connection->errorInfo()), terminateAfterLog: true);
+          }
+        }
+      }
+
+      if (empty($ranMigrations))
+      {
+        Logger::log(message: 'Nothing to do', terminateAfterLog: true);
+      }
+      Logger::logUpdate(path: '__assegai_schema_migrations');
+    }
+    catch(PDOException $e)
+    {
+      Logger::error($e->getMessage(), terminateAfterLog: true);
+    }
   }
 
-  public function revert(): void
+  public function revert(string $mode = self::RUN_ALL): void
   {
 
   }
 
   public function redo(): void
   {
-    $this->revert();
-    $this->run();
+    $this->revert(mode: self::REVERT_NEXT);
+    $this->run(mode: self::RUN_NEXT);
   }
 
   public function listMigrations(): array|false
@@ -98,8 +146,10 @@ final class Migrator
       $allMigrations = scandir($this->migrationDirectory);
       $allMigrations = array_splice($allMigrations, 2);
       $alreadyRan = $this->getRanMigrations();
+      $titleColor = Color::YELLOW;
+      $resetColor = Color::RESET;
 
-      echo "\n\e[0;33mMigrations:\e[0m\n";
+      echo "\n${titleColor}Migrations:${resetColor}\n";
       
       foreach ($allMigrations as $item)
       {
