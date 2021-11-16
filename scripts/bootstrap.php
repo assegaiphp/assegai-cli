0451e079f1f679d288b04699228f8e927c962e8e
Andrew Masiye <amasiye313@gmail.com>
2021-11-16 15:19:45 +0200

    refactor(bootstrap): add util functions
    * add function to check for valid workspaces
    * add function to prompt users for input

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -1,5 +1,10 @@
 #!/usr/bin/php
 <?php
+
+use Assegai\LIB\Color;
+use Assegai\LIB\Logging\Logger;
+use Assegai\LIB\WorkspaceManager;
+
 $args = array_slice($argv, 1);
 $workingDirectory = exec(command: 'pwd');
 $assegaiPath = exec(command: "which assegai");
@@ -20,3 +25,38 @@ spl_autoload_register(function ($class) {
     require_once  $filename;
   }
 });
+
+/**
+ * Checks the `workding directory` for an `assegai.json` file. If no 
+ * project file is found it will log an error.
+ * 
+ * @param null|string $commandName The name of the command that requires 
+ * an assegai workspace to run.
+ */
+function checkWorkspace(?string $commandName = null): void
+{
+  if (is_null($commandName))
+  {
+    $commandName = substr(basename(__FILE__), 0, -4);
+  }
+
+  if (!WorkspaceManager::isAssegaiWorkspace())
+  {
+    Logger::error("The $commandName command requires to be run in an Assegai workspace, but a project definition could not be found.", terminateAfterLog: true);
+  }
+}
+
+function prompt(string $message = 'Enter choice', ?string $defaultValue = null): string
+{
+  $defaultHint = '';
+  if (!empty($defaultValue))
+  {
+    $defaultHint = "($defaultValue)";
+  }
+
+  printf("%s?%s %s: %s%s", Color::GREEN, Color::RESET, $message, $defaultHint, Color::BLUE);
+  $line = trim(fgets(STDIN));
+  echo Color::RESET;
+
+  return $line;
+}
