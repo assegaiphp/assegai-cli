32bcddb10c375fe112900bf1dff0702bc0a4d226
Andrew Masiye <amasiye313@gmail.com>
2021-11-18 07:30:37 +0200

    feat(command): add commands

--- src/help.php
+++ src/help.php
@@ -22,12 +22,13 @@ $mainMenu = new Menu(
 );
 $mainMenu->addRange([
   new MenuItem(value: 'config', description: 'Retrieves or sets Assegai configuration values in the assegai.json file for the workspace.'),
-  new MenuItem(value: 'database', alias: 'd', description: 'Manage configured database schemas.'),
+  new MenuItem(value: 'database', alias: 'd', description: 'Manages configured database schemas.'),
   new MenuItem(value: 'generate', alias: 'g', description: 'Generates and/or modifies files based on a schematic.'),
-  new MenuItem(value: 'info', description: 'Display Assegai project details.'),
-  new MenuItem(value: 'init', description: 'Create an empty Assegai workspace or reinitialize an existing one.'),
-  new MenuItem(value: 'migration', description: 'Manage database migrations.'),
-  new MenuItem(value: 'new', description: 'Generate Assegai application.'),
+  new MenuItem(value: 'info', description: 'Displays Assegai project details.'),
+  new MenuItem(value: 'init', description: 'Creates an empty Assegai workspace or reinitialize an existing one.'),
+  new MenuItem(value: 'lint', description: 'Runs the code linter.'),
+  new MenuItem(value: 'migration', description: 'Manages database migrations.'),
+  new MenuItem(value: 'new', description: 'Generates Assegai application.'),
   new MenuItem(value: 'test', alias: 't', description: 'Runs unit tests in a project.'),
   new MenuItem(value: 'update', alias: 'u', description: 'Updates your application and its dependencies. See https://update.assegai.ml/'),
   new MenuItem(value: 'version', alias: 'v', description: 'Outputs Assegai CLI version.'),
---
+++ src/test.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+
+use Assegai\LIB\Color;
+
+require_once 'bootstrap.php';
+
+if (file_exists("$workingDirectory/run-unittest"))
+{
+  printf("%sRunning Unit Tests...%s\n\n", Color::BLUE, Color::RESET);
+  echo shell_exec('./run-unittest') . "\n";
+}
