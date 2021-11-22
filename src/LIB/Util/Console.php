28746e6552577e9bbe798a85cbea7458655881bb
Andrew Masiye <amasiye313@gmail.com>
2021-11-22 05:50:57 +0200

    feat(util): add console control classes

---
+++ src/LIB/Util/Console.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+
+namespace Assegai\LIB\Util;
+
+use Assegai\LIB\Color;
+
+final class Console
+{
+  public static function color(): Color
+  {
+    return new Color;
+  }
+
+  public static function cursor(): ConsoleCursor
+  {
+    return new ConsoleCursor;
+  }
+
+  public static function eraser(): ConsoleEraser
+  {
+    return new ConsoleEraser;
+  }
+
+  public static function info(): TermInfo
+  {
+    return new TermInfo;
+  }
+}
