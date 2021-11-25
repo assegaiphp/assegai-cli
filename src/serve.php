a2435e87d2bf63b7f6f85cd21dbfc97e25301290
Andrew Masiye <amasiye313@gmail.com>
2021-11-25 06:02:36 +0200

    feat(command): add serve command
    * add command to start local dev server

---
+++ src/serve.php
@@ -0,0 +1,43 @@
+#!/usr/bin/env php
+<?php
+
+use Assegai\CLI\LIB\Color;
+use Assegai\CLI\LIB\Logging\Logger;
+use Assegai\CLI\LIB\Util\Config;
+
+array_shift($args);
+list($path) = match(count($args)) {
+  0       => [null],
+  default => $args
+};
+
+$config = Config::get(path: $path);
+$host = 'localhost';
+$port = '3000';
+
+if (empty($config))
+{
+  exit;
+}
+
+if (
+  isset($config->development) &&
+  isset($config->development->server)
+)
+{
+  $server = $config->development->server;
+  if (isset($server->host))
+  {
+    $host = $server->host;
+  }
+
+  if (isset($server->port))
+  {
+    $port = $server->port;
+  }
+}
+
+$command = "php -S $host:$port";
+
+Logger::log(message: sprintf("Starting Server...\n%sListening on port %s\n", Color::YELLOW, $port));
+$response = exec($command);
