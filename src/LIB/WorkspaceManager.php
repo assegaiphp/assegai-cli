8f5b15100e0f9d50fe9295033b79d74e3aff7b9f
Andrew Masiye <amasiye313@gmail.com>
2021-12-26 05:51:44 +0200

    feat(lib): add init static method to WorkspaceManager

--- src/LIB/WorkspaceManager.php
+++ src/LIB/WorkspaceManager.php
@@ -7,6 +7,94 @@ use Assegai\CLI\LIB\Logging\Logger;
 
 final class WorkspaceManager
 {
+  public static function init(
+    ?string $path = null,
+    ?string $name = null,
+    ?string $description = null,
+    ?string $version = null,
+    ?string $project_type = null,
+    ?string $root = '',
+    ?string $source_root = 'src',
+    ?array &$output = []
+  ): void
+  {
+    global $workingDirectory, $assegaiPath;
+
+    $shortOptions = '';
+
+    $longOptions = [
+      'path:',
+      'name:',
+      'description:',
+      'version:',
+      'project_type:',
+      'root:',
+      'source_root:',
+      'prefix:',
+      'scripts:',
+      'development:',
+    ];
+    $restIndex = 2;
+
+    $suggestedProjectName = basename($workingDirectory);
+
+    $options = getoptions(shortOptions: $shortOptions, longOptions: $longOptions, restIndex: $restIndex, offset: 1);
+    extract($options);
+
+    if (!$name)
+    {
+      $name = prompt(message: 'Project name', defaultValue: $suggestedProjectName);
+    }
+
+    if (!$description)
+    {
+      $description = prompt(message: 'Description');
+    }
+
+    if (!$version)
+    {
+      $version = prompt(message: 'Version', defaultValue: '0.0.0');
+    }
+
+    if (!$project_type)
+    {
+      $project_type = prompt(message: 'Project Type', defaultValue: 'application');
+    }
+
+    $filepath = $path ?? exec('pwd');
+    $sourceFilename = "$assegaiPath/templates/init/assegai.template";
+
+    if (!file_exists($sourceFilename))
+    {
+      Logger::error(message: "File not found: " . basename($sourceFilename), exit: true);
+    }
+
+    $templateContent = file_get_contents($sourceFilename);
+    $assegaiJson = str_replace('NAME', $name, $templateContent);
+    $assegaiJson = str_replace('DESCRIPTION', $description, $assegaiJson);
+    $assegaiJson = str_replace('VERSION', $version, $assegaiJson);
+    $assegaiJson = str_replace('PROJECT_TYPE', $project_type, $assegaiJson);
+    $assegaiJson = str_replace('ROOT', $root, $assegaiJson);
+    $assegaiJson = str_replace('SOURCE', $source_root, $assegaiJson);
+    $output['name']         = $name;
+    $output['description']  = $description;
+    $output['version']      = $version;
+    $output['project_type'] = $project_type;
+    $output['root']         = $root;
+    $output['source']       = $source;
+
+    $filename = "$filepath/assegai.json";
+    $bytes = file_put_contents($filename, $assegaiJson);
+
+    if ($bytes === false)
+    {
+      Logger::error(message: 'Failed to created ' . basename($bytes), exit: true);
+    }
+
+    echo "\n";
+    Logger::logCreate(path: basename($filename), filesize: $bytes);
+  }
+
   public static function isAssegaiWorkspace(): bool
   {
     global $workingDirectory;
