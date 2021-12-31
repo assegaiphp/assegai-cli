150883e9ecb16ef64beec6503cf30a448c8eef7a
Andrew Masiye <amasiye313@gmail.com>
2021-12-31 19:16:27 +0200

    fix(): Fix arguments not being passed to test command Fixes #5

--- src/test.php
+++ src/test.php
@@ -5,8 +5,15 @@ use Assegai\CLI\LIB\Color;
 
 require_once 'bootstrap.php';
 
+array_shift($args);
+$argsString = implode(' ', $args);
+if (!empty($argsString))
+{
+  $argsString = ' ' . $argsString;
+}
+
 if (file_exists("$workingDirectory/run-unittest"))
 {
   printf("%sRunning Unit Tests...%s\n\n", Color::BLUE, Color::RESET);
-  echo shell_exec('./run-unittest') . "\n";
+  echo shell_exec('./run-unittest' . $argsString) . "\n";
 }
