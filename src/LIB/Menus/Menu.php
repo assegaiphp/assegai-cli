dd076d5efcf66a2ffffac0529ccd4e025cc66c65
Andrew Masiye <amasiye313@gmail.com>
2022-01-04 17:57:24 +0200

    fix(lib): fix selected item issue in prompt method

--- src/LIB/Menus/Menu.php
+++ src/LIB/Menus/Menu.php
@@ -10,6 +10,9 @@ class Menu
 {
   private ?MenuItem $selected = null;
 
+  /**
+   * @param array<int, MenuItem> $items
+   */
   public function __construct(
     private string $title,
     private array $items = [],
@@ -169,22 +172,23 @@ class Menu
     return trim("${description}${titleColorCode}" . $this->title . "\e[0m\n$itemsOutput") . "\n";
   }
 
-  public function prompt(string $message = 'Choose option', bool $useKeypad = false): ?MenuItem
+  public function prompt(string $message = 'Choose option', bool $useKeypad = false, bool $multiSelect = false): ?MenuItem
   {
     global $assegaiPath;
 
     if ($useKeypad)
     {
-      $options = [$this->title()];
+      $options = [];
 
       foreach ($this->items as $item)
       {
         $options[] = $item->value();
       }
 
-      $response = promptSelect(options: $options, message: $message);
+      $selectedIndex = 0;
+      $response = promptSelect(options: $options, message: $message, selectedIndex: $selectedIndex);
 
-      $this->selected = $this->items[$response];
+      $this->selected = $this->items[($selectedIndex + 1)];
     }
     else
     {
