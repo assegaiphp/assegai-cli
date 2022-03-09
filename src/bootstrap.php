cbd801ec480f94b5d5fd964454298644efa54e47
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-03-09 02:42:54 +0200

    fix(lib): fix project type selection bug Fixes #37

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -40,22 +40,22 @@ spl_autoload_register(function ($class) {
 # List options
 $mainMenu = new Menu(
   title: 'Available Commands',
+  items: [
+    new MenuItem(value: 'config', description: 'Retrieves or sets Assegai configuration values in the assegai.json file for the workspace.'),
+    new MenuItem(value: 'database', alias: 'd', description: 'Manages configured database schemas.'),
+    new MenuItem(value: 'generate', alias: 'g', description: 'Generates and/or modifies files based on a schematic.'),
+    new MenuItem(value: 'info', description: 'Displays Assegai project details.'),
+    new MenuItem(value: 'init', description: 'Creates an empty Assegai workspace or reinitializes an existing one.'),
+    new MenuItem(value: 'lint', description: 'Runs the code linter.'),
+    new MenuItem(value: 'migration', description: 'Manages database migrations.'),
+    new MenuItem(value: 'new', description: 'Generates a new Assegai application.'),
+    new MenuItem(value: 'serve', description: 'Starts a local development server.'),
+    new MenuItem(value: 'test', alias: 't', description: 'Runs unit tests in a project.'),
+    new MenuItem(value: 'update', alias: 'u', description: 'Updates your application and its dependencies. See https://update.assegai.ml/'),
+    new MenuItem(value: 'version', alias: 'v', description: 'Outputs Assegai CLI version.'),
+  ],
   options: new MenuOptions(
     showDescriptions: true,
     showIndexes: false
   )
 );
-$mainMenu->addRange([
-  new MenuItem(value: 'config', description: 'Retrieves or sets Assegai configuration values in the assegai.json file for the workspace.'),
-  new MenuItem(value: 'database', alias: 'd', description: 'Manages configured database schemas.'),
-  new MenuItem(value: 'generate', alias: 'g', description: 'Generates and/or modifies files based on a schematic.'),
-  new MenuItem(value: 'info', description: 'Displays Assegai project details.'),
-  new MenuItem(value: 'init', description: 'Creates an empty Assegai workspace or reinitializes an existing one.'),
-  new MenuItem(value: 'lint', description: 'Runs the code linter.'),
-  new MenuItem(value: 'migration', description: 'Manages database migrations.'),
-  new MenuItem(value: 'new', description: 'Generates a new Assegai application.'),
-  new MenuItem(value: 'serve', description: 'Starts a local development server.'),
-  new MenuItem(value: 'test', alias: 't', description: 'Runs unit tests in a project.'),
-  new MenuItem(value: 'update', alias: 'u', description: 'Updates your application and its dependencies. See https://update.assegai.ml/'),
-  new MenuItem(value: 'version', alias: 'v', description: 'Outputs Assegai CLI version.'),
-]);
