f96b2aa529dbe66198959fcd58d357c4dd2900b6
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-03-21 12:15:21 +0200

    refactor(): enum option to generate menu

--- src/Commands/Generate/class.php
+++ src/Commands/Generate/class.php
@@ -2,3 +2,13 @@
 <?php
 
 namespace Assegai\CLI\Commands\Generate;
+
+use Assegai\CLI\LIB\Generation\SchematicBuilder;
+
+list($name) = match (count($args)) {
+  1 => [null],
+  default => array_slice($args, 1)
+};
+
+$schematicBuilder = new SchematicBuilder;
+$schematicBuilder->buildClass(path: $name);
