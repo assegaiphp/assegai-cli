25899748261c5b4c90b831308b90f6b92ca79a35
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-03-21 12:03:30 +0200

    feat(commands): create enum command

---
+++ src/Commands/Generate/enum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Assegai\CLI\Commands\Generate;
+
+use Assegai\CLI\LIB\Generation\SchematicBuilder;
+
+list($name) = match (count($args)) {
+  1 => [null],
+  default => array_slice($args, 1)
+};
+
+$schematicBuilder = new SchematicBuilder;
+$schematicBuilder->buildEnum(name: $name);
