c1cf7d91dda431de50e1bd7b794d09c0606ac9ac
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-04-14 22:38:30 +0200

    fix(): fix missing user module in new project routes

--- templates/project/app/routes.php
+++ templates/project/app/routes.php
@@ -3,8 +3,10 @@
 use AssegaiPHP\Modules\Authentication\AuthenticationModule;
 use Assegai\Core\Routing\Route;
 use AssegaiPHP\Modules\Home\HomeModule;
+use AssegaiPHP\Modules\Users\UsersModule;
 
 return [
   new Route(path: '/', module: HomeModule::class),
   new Route(path: 'authentication', module: AuthenticationModule::class),
+  new Route(path: 'users', module: UsersModule::class),
 ];
